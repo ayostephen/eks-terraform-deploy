8d4559f402c572bd01c8e95e5cb56fdacfc7b404
ayostephen <adejuwonstephenayo@gmail.com>
2024-10-10 21:30:34 +0100

    update-kubeconfig.sh

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
-response="$(aws eks list-clusters --region us-west-2 --output text | grep -i dominion-cluster 2>&1)" 
-if [[ $? -eq 0 ]]; then
-    echo "Success: Dominion-cluster exist"
-    aws eks update-kubeconfig --name dominion-cluster --region us-west-2 && export KUBE_CONFIG_PATH=~/.kube/config
+# #!/bin/bash
+# response="$(aws eks list-clusters --region us-west-2 --output text | grep -i dominion-cluster 2>&1)" 
+# if [[ $? -eq 0 ]]; then
+#     echo "Success: Dominion-cluster exist"
+#     aws eks update-kubeconfig --name dominion-cluster --region us-west-2 && export KUBE_CONFIG_PATH=~/.kube/config
 
+# else
+#     echo "Error: Dominion-cluster does not exist"
+# fi
+
+# Variables (replace with your values or environment variables)
+CLUSTER_NAME="dominion-cluster"
+REGION="region us-west-2"
+
+# Update the kubeconfig file using the AWS CLI
+aws eks update-kubeconfig --name "$CLUSTER_NAME" --region "$REGION"
+
+# Verify kubeconfig update
+if [ $? -eq 0 ]; then
+  echo "Kubeconfig updated successfully."
 else
-    echo "Error: Dominion-cluster does not exist"
+  echo "Failed to update kubeconfig."
+  exit 1
 fi
